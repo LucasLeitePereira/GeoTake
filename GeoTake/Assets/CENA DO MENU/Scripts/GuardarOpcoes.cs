d849e48acba522071861485211e58492e779692a
jhonxl <108690886+jhonxl@users.noreply.github.com>
2025-04-16 16:48:47 -0300

    Adicionando Opções ao Menu

---
+++ GeoTake/Assets/CENA DO MENU/Scripts/GuardarOpcoes.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardarOpcoes : MonoBehaviour
+{
+    public int numeroJogadores;
+    void Start()
+    {
+
+    }
+
+    public void umJogador()
+    {
+        numeroJogadores = 1;
+    }
+    void Update()
+    {
+        
+    }
+}
