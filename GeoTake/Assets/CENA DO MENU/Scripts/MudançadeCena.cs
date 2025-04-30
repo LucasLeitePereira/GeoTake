cb1cf90d4c92d3014f6ee907031673aa23b55c5f
andreelzs <163312245+andreelzs@users.noreply.github.com>
2025-04-30 17:02:31 -0300

    Revert "Merge branch 'ANdré'"
    This reverts commit 0a72e438c99de80351e70bf15a63630972ba4664, reversing
    changes made to 489d0b563435a30a27af85020bf8f4eb1dbe9aea.

--- GeoTake/Assets/CENA DO MENU/Scripts/MudançadeCena.cs
+++ GeoTake/Assets/CENA DO MENU/Scripts/MudançadeCena.cs
@@ -6,13 +6,10 @@ using UnityEngine.SceneManagement;
 public class MudançadeCena : MonoBehaviour
 {
     [SerializeField] private string CenaJogar;
+    [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
     [SerializeField] private GameObject SelectPlayer;
-    [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject SelectDificuldade;
-    [SerializeField] private string CarregarJogo;
-    [SerializeField] private GameObject TelaPerdeu;
-    [SerializeField] private GameObject TelaGanhou;
 
     string currentScene;
 
@@ -32,12 +29,6 @@ public class MudançadeCena : MonoBehaviour
     {
         Debug.Log("Inciando o jogo");
         SceneManager.LoadScene(CenaJogar);
-    }
-
-    public void CarregarFase()
-    {
-        Debug.Log("Inciando o jogo");
-        SceneManager.LoadScene(CarregarJogo);
     }
 
     public void ReiniciarLevel()
@@ -80,15 +71,6 @@ public class MudançadeCena : MonoBehaviour
         painelMenuInicial.SetActive(false);
         SelectDificuldade.SetActive(true);
     }
-
-    public void AbrirOpcoesPerdeu()
-    {
-        TelaPerdeu.SetActive(false);
-
-    }
-
-
-
 
 }
 
