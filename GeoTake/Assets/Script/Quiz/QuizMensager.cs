9fc3f6a458a77b6c03c728f0c3298d507e2bdf43
Matheus gomes <matheusgomesdacosta22@gmail.com>
2025-05-02 12:25:39 -0300

    ajustes e quiz

---
+++ GeoTake/Assets/Script/Quiz/QuizMensager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class QuizMensager : MonoBehaviour
+{
+    public TMP_Text cabecalho;
+    public Button[] alternativa;
+    public Quiz[] quantidade;
+    public int index = 0;
+    public string cena;
+    void Start()
+    {
+        MostrarQuiz();
+    }
+
+    public void MostrarQuiz()
+    {
+       
+        Quiz p = quantidade[index];
+        cabecalho.text = p.perguntaTexto;
+        for(int i = 0; i < alternativa.Length; i++)
+        {
+            alternativa[i].GetComponentInChildren<TextMeshProUGUI>().text = p.alternativasTexto[i];
+
+            int possibilidade = i;
+            alternativa[i].onClick.RemoveAllListeners();
+            alternativa[i].onClick.AddListener(() => verificar(possibilidade));
+        }
+    }
+    public void verificar(int possibilidade)
+    {
+        Quiz p = quantidade[index];
+        if (possibilidade == p.indexCorreto)
+        {
+            Debug.Log("CORRETO");
+        }
+        else
+        {
+            Debug.Log("Errado");
+        }
+
+        ++index;
+        if (index < 4)
+        {
+             MostrarQuiz();
+        }
+        else
+        {
+            SceneManager.LoadScene(cena);
+        }
+       
+    }
+   
+}
