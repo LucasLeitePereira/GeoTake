65256aae4d7cdb2133da4c4a0e65e67548259958
andreelzs <163312245+andreelzs@users.noreply.github.com>
2025-05-07 14:53:32 -0300

    Revert "PlayerPrefs"
    This reverts commit 6494f743289b26bd658c3134eb697c9371498d25.

--- GeoTake/Assets/CENA DO MENU/Scripts/MudançadeCena.cs
+++ GeoTake/Assets/CENA DO MENU/Scripts/MudançadeCena.cs
@@ -6,13 +6,10 @@ using UnityEngine.SceneManagement;
 public class MudançadeCena : MonoBehaviour
 {
     [SerializeField] private string CenaJogar;
+    [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject painelOpcoes;
     [SerializeField] private GameObject SelectPlayer;
-    [SerializeField] private GameObject painelMenuInicial;
     [SerializeField] private GameObject SelectDificuldade;
-    [SerializeField] private string CarregarJogo;
-    [SerializeField] private GameObject TelaPerdeu;
-    [SerializeField] private GameObject TelaGanhou;
 
     string currentScene;
     public void Start()
@@ -25,12 +22,6 @@ public class MudançadeCena : MonoBehaviour
     {
         Debug.Log("Inciando o jogo");
         SceneManager.LoadScene(CenaJogar);
-    }
-
-    public void CarregarFase()
-    {
-        Debug.Log("Inciando o jogo");
-        SceneManager.LoadScene(CarregarJogo);
     }
 
     public void ReiniciarLevel()
@@ -73,15 +64,6 @@ public class MudançadeCena : MonoBehaviour
         painelMenuInicial.SetActive(false);
         SelectDificuldade.SetActive(true);
     }
-
-    public void AbrirOpcoesPerdeu()
-    {
-        TelaPerdeu.SetActive(false);
-
-    }
-
-
-
 
 }
 
