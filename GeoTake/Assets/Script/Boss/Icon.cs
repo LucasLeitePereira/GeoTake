7cfc3278cf575d03ac32d7e85fb19bfe31550e2b
Matheus gomes <matheusgomesdacosta22@gmail.com>
2025-05-07 21:56:10 -0300

    ajuste icon

--- GeoTake/Assets/Script/Boss/Icon.cs
+++ GeoTake/Assets/Script/Boss/Icon.cs
@@ -6,18 +6,21 @@ public class Icon : MonoBehaviour
 {
     public static Icon ic;
     private Vector3 originalScale;
+    private bool verificar = true;
     private float temp;
-    public bool validar = true;
-    void Start()
+    void OnEnable()
     {
-        // Armazena o tamanho original do objeto
+       
         originalScale = transform.localScale;
+        if(verificar == true){
         StartCoroutine(ScaleRoutine());
+        verificar = false;
+        }
     }
      void Update()
     {
-          temp += Time.deltaTime;
-        if(temp > 9 && validar == true)
+          temp = Mathf.RoundToInt(Time.time);
+        if(temp %15 == 0 && verificar == false)
         {
             StartCoroutine(ScaleReduz());
           
@@ -25,21 +28,21 @@ public class Icon : MonoBehaviour
     }
     IEnumerator ScaleRoutine()
     {
-        // Começa invisível
+       
         transform.localScale = Vector3.zero;
 
-        // Aumenta até o tamanho original
+       
         float t = 0;
         while (t < 1f)
         {
-            t += Time.deltaTime * 2f; // 0.5s total
+            t += Time.deltaTime * 2f; 
             transform.localScale = Vector3.Lerp(Vector3.zero, originalScale, t);
             yield return null;
         }
 
     }
     IEnumerator ScaleReduz() {
-        // Diminui até 0
+       
        float t = 0f;
         while (t < 1f)
         {
@@ -47,14 +50,9 @@ public class Icon : MonoBehaviour
             transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, t);
             yield return null;
         }
-
-
-        if (t > 1f)
-        {
-            validar = false;
-            temp = 0;
+            verificar = true;
             gameObject.SetActive(false);
-        }
+        
     }
 }
 
