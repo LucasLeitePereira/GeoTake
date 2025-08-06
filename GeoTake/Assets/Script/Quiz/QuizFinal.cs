897537fd192f0e7918156fe2ecc0c2f0dd780f13
Matheus gomes <matheusgomesdacosta22@gmail.com>
2025-08-06 17:06:53 -0300

    quiz

--- GeoTake/Assets/Script/Quiz/QuizFinal.cs
+++ GeoTake/Assets/Script/Quiz/QuizFinal.cs
@@ -7,57 +7,88 @@ using UnityEngine.SceneManagement;
 
 public class QuizFinal : MonoBehaviour
 {
-    public GameObject resultados;
     public TMP_Text cabecalho;
     public Button[] alternativa;
     public Quiz[] quantidade;
+    public Button botaoProxima;
+    public GameObject painelFinal;
+    public TMP_Text textoResultado;
     public int index = 0;
     public string cena;
+
+    private Color corOriginal;
+    private int acertos = 0;
+
     void Start()
     {
+        index = 0;
+        acertos = 0;
+        corOriginal = alternativa[0].GetComponent<Image>().color;
+        botaoProxima.gameObject.SetActive(false);
         MostrarQuiz();
     }
 
     public void MostrarQuiz()
     {
-
         Quiz p = quantidade[index];
         cabecalho.text = p.perguntaTexto;
+        botaoProxima.gameObject.SetActive(false);
+
         for (int i = 0; i < alternativa.Length; i++)
         {
+            alternativa[i].interactable = true;
+            alternativa[i].GetComponent<Image>().color = corOriginal;
             alternativa[i].GetComponentInChildren<TextMeshProUGUI>().text = p.alternativasTexto[i];
 
             int possibilidade = i;
             alternativa[i].onClick.RemoveAllListeners();
             alternativa[i].onClick.AddListener(() => verificar(possibilidade));
-
         }
+
+        botaoProxima.onClick.RemoveAllListeners();
+        botaoProxima.onClick.AddListener(ProximaPergunta);
     }
+
     public void verificar(int possibilidade)
     {
+        foreach (var btn in alternativa)
+            btn.interactable = false;
+
         Quiz p = quantidade[index];
-        if (possibilidade == p.indexCorreto)
+
+        for (int i = 0; i < alternativa.Length; i++)
         {
-            Debug.Log("CORRETO");
-        }
-        else
-        {
-            Debug.Log("Errado");
+            Image img = alternativa[i].GetComponent<Image>();
+            if (i == p.indexCorreto)
+                img.color = Color.green;
+            else if (i == possibilidade)
+                img.color = Color.red;
+            else
+                img.color = Color.red;
         }
 
-        if (index < 0)
+        if (possibilidade == p.indexCorreto)
+            acertos++;
+
+        botaoProxima.gameObject.SetActive(true);
+    }
+
+    public void ProximaPergunta()
+    {
+        index++;
+        if (index < quantidade.Length)
         {
-           
             MostrarQuiz();
-            
         }
-        
         else
         {
-            resultados.SetActive(true);
-            this.gameObject.SetActive(false);
+            MostrarResultadoFinal();
         }
-        
     }
-   
+
+    void MostrarResultadoFinal()
+    {
+        painelFinal.SetActive(true);
+        textoResultado.text = $"Você acertou {acertos} de 15 perguntas!";
+    }
 }
